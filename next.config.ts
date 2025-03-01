cd63beb080b4f4a6885ffadbd24d9771a128aa65
tomaslau <hi@tomaslau.com>
2025-03-01 09:19:41 +0100

    clean up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  distDir: '.next',
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
